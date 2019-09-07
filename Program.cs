2bac0356b4d1a54fa819cce064d7734fdee5896f
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-09-07 16:45:17 +0700

    Add: simple c# protobuf code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Google.Protobuf;
+
+namespace CsharpProtobuf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loginPacket = new LoginPacket
+            {
+                Code = 0x01,
+                Size = 29,
+                SizeOfUsername = 8,
+                Username = "imkk-000",
+                SizeOfPassword = 13,
+                Password = "p@55w0rd1234*",
+            };
+            // convert it to bytes
+            System.IO.File.WriteAllBytes("send.bin", loginPacket.ToByteArray());
+        }
+    }
+}
